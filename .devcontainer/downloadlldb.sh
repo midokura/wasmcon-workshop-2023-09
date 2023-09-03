02426a3361c23aeedbf3705ffb8f2db6a587184c
ayakoakasaka <98828539+ayakoakasaka@users.noreply.github.com>
2023-09-03 10:26:25 +0000

    download lldb debugger beforehand

---
+++ .devcontainer/downloadlldb.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+# Download LLDB debugger
+curl -sL https://github.com/bytecodealliance/wasm-micro-runtime/releases/download/WAMR-1.2.3/wamr-lldb-1.2.3-x86_64-ubuntu-22.04.tar.gz | tar zxvf - -C /tmp && \
+mv /tmp/wamr-lldb/* ~/.vscode-remote/extensions/wamr-ide.wamride-1.2.3/resource/debug/linux/
+
+
